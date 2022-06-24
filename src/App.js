784f3d3e8cdf7ef4cd336fa7c4fb3b4eb1ba19f8
RuslanBets <rbec4263@gmail.com>
2022-06-24 18:21:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: props.number
+    }
+  }
+
+  incFn = () => {
+    this.setState(({number}) => number === 50? 50 : {number: number + 1})
+  }
+
+  decFn = () => {
+    this.setState(({number}) => {
+      if (number === -50) {
+        return
+      } else {
+        return {
+          number: number - 1
+        }
+      }
+    })
+  }
+
+  rndFn = () => {
+    this.setState(({number}) => {
+      let randomNum = Math.floor(Math.random() * (50 - -50)) + -50
+      return {
+        number: randomNum
+      }
+    })
+  }
+
+  resetFn = () => {
+    this.setState(({number}) => ({number : this.props.number}))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <span className='number'>{this.state.number}</span>
+          <div className="wrapperBtns">
+            <button onClick= {this.incFn}>INC</button>
+            <button onClick={this.decFn}>DEC</button>
+            <button onClick={this.rndFn}>RND</button>
+            <button onClick={this.resetFn}>RESET</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
